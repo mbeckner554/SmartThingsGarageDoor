4bc9f0efce143d49ffe306691dd8240794129f9d
mbeckner554 <michael.beckner@gmail.com>
2017-02-07 20:39:48 -0500

    formatting

--- ESP8266Switch.groovy
+++ ESP8266Switch.groovy
@@ -10,8 +10,8 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *  Derived from Generic HTTP Device
- * 	- Author: JZ
- *  - https://github.com/JZ-SmartThings/SmartThings/blob/master/Devices/Generic%20HTTP%20Device/GenericHTTPDevice.groovy
+ *		- Author: JZ
+ *		- https://github.com/JZ-SmartThings/SmartThings/blob/master/Devices/Generic%20HTTP%20Device/GenericHTTPDevice.groovy
  *  
 */
 
